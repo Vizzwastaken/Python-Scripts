a8732352dc99b86f99ba6674e50e3499248c458a
Vizzwastaken <91386217+Vizzwastaken@users.noreply.github.com>
2024-05-21 22:06:45 +0530

    Update Directory_walk.py

--- Directory_walk.py
+++ Directory_walk.py
@@ -1,14 +1,17 @@
 import os
-folderlist = input("Please enter the folder paths : ").split(" ") # user enters the list of folder paths
+# Prompting user to input folder paths and splitting into a list
+folderlist = input("Please enter the folder paths: ").split(" ")
+# Iterating over each folder path provided
 for folder in folderlist:
     try:
-        if os.path.exists(folder):
-            for dirpath, subdir, files in os.walk(folder): #dirpath=Directory path , subdir=sub dir under the dir, files = files in each dir
-                print("Directory :" + dirpath)
+        if os.path.exists(folder):  # Checking if folder exists
+            # Walking through directory tree and printing file names
+            for dirpath, subdir, files in os.walk(folder):
+                print("Directory: " + dirpath)
                 for name in files:
                     print(name)
         else:
-            print(folder + " Doesnt exist")
+            print(folder + " doesn't exist")  # If folder doesn't exist
     except PermissionError:
-        print("Permission denied to " + folder)
-        break
+        print("Permission denied to " + folder)  # Handling permission errors
+        break  # Exiting loop if permission denied to any folder
