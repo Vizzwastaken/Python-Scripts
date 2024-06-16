7bbd2fb489fde8506fd5a2d221d29dfef8f3f280
Vizzwastaken <91386217+Vizzwastaken@users.noreply.github.com>
2024-06-16 21:17:59 +0530

    Create Automate.py

---
+++ Test-Automation/Automate.py
@@ -0,0 +1,14 @@
+import os
+
+folder = "."
+for dirpath, subdir, files in os.walk(folder):
+    if dirpath != '.':
+        print(dirpath)
+        os.chdir(dirpath)
+        for name in files:
+            command1 = f"chmod +x {name}"
+            os.system(command1)
+            exec = f"python {name}"
+            os.system(exec)
+        command2 = "ls -lrt"
+        os.system(command2)
